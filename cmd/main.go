cfff4a0534ef96be4be4b83106a30abf05a4ee38
Kiran Kumar Mohanty <nityo_kiran.mohanty@nfcguest.com>
2023-08-26 17:31:29 +0530

    remove comment

--- cmd/main.go
+++ cmd/main.go
@@ -11,13 +11,6 @@ import (
 )
 
 func main() {
-	// Create a loop that will continue to prompt the user for a word until they enter "exit"
-	// For each word, call the dictionaryQuery.Query() method and print the result
-	// If the word is not found, print "Word not found"
-	// If there is an error, print "Error: <error message>"
-	// If the user enters "exit", print "Goodbye!" and exit the program
-	// If the user enters anything else, print "Invalid input. Please try again."
-
 	fmt.Println("Welcome to the Lexi Query!")
 
 	dq := usecase.NewDictionaryQuery(config.GetCollegiateURL(), config.GetAPIKey())
